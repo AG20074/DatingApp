7db3d2b3116391e7da0d2431527559319c9a5e6e
Akalu <akalu.tadesse@hotmail.com>
2018-11-23 10:50:03 -0600

    End of Section 8

--- DatingApp.API/Migrations/20181122194744_test.cs
+++ DatingApp.API/Migrations/20181122194744_test.cs
@@ -2,16 +2,21 @@
 
 namespace DatingApp.API.Migrations
 {
-    public partial class AddedUserClass : Migration
+    public partial class test : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.AddColumn<string>(
+                name: "MI",
+                table: "Users",
+                nullable: true);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropColumn(
+                name: "MI",
+                table: "Users");
         }
     }
 }
